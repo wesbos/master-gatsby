38fcf28cd33180e5085efffa407a06fedc7cc03c
Wes Bos <wesbos@gmail.com>
2020-05-08 16:47:26 -0400

    fix the font issue

--- gatsby/src/styles/Typography.js
+++ gatsby/src/styles/Typography.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-import font from '../assets/fonts/FrenchFriesILTD-Regular.woff';
+import font from '../assets/fonts/frenchfries.woff';
 
 const Typography = createGlobalStyle`
   @font-face {

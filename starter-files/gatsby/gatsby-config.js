6aa0f49f46647fa37f7b9ee16799ac778d7f7115
Wes Bos <wesbos@gmail.com>
2020-09-21 10:21:38 -0400

    add empty gatsby-config.js file

---
+++ starter-files/gatsby/gatsby-config.js
@@ -0,0 +1,1 @@
+// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.

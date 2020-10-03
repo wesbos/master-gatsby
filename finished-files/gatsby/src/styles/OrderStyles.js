66072fbc00f4af241de57815b33150230e8cde76
Juliette Rapala <jrapala@gmail.com>
2020-10-03 11:04:33 -0500

    fix: adjust OrderStyles styles

--- finished-files/gatsby/src/styles/OrderStyles.js
+++ finished-files/gatsby/src/styles/OrderStyles.js
@@ -8,9 +8,14 @@ const OrderStyles = styled.form`
     grid-column: span 2;
     max-height: 600px;
     overflow: auto;
-    display: grid;
-    gap: 1rem;
-    align-content: start;
+    label {
+      display: grid;
+      gap: 1rem;
+      align-content: start;
+    }
+    label + label {
+      margin-top: 1rem;
+    }
     &.order,
     &.menu {
       grid-column: span 1;

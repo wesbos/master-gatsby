1c0d4016e821262f8dac56c8f64da8a9ed6b7691
David Lee <davideuijinlee@gmail.com>
2020-11-03 19:21:28 -0800

    global styles added

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout({children}) {
     return (
         <div>
+            <GlobalStyles />
             <Nav />
             {children}
             <Footer />

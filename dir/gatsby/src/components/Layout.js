8e8e1d433646e8bffd0d8a5992d4a7e0e5de262c
itay haephrati <itay1313@gmail.com>
2021-07-14 00:20:09 +0300

    adding style

--- dir/gatsby/src/components/Layout.js
+++ dir/gatsby/src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <link rel="stylesheet" href="./" />
       <Nav />
       {children}
       <Footer />

64432a8c90e3cee37aabb8739bef2b6227b4d092
itay haephrati <itay1313@gmail.com>
2021-07-14 15:19:22 +0300

    logo

--- dir/gatsby/src/components/Layout.js
+++ dir/gatsby/src/components/Layout.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <link rel="stylesheet" href="./" />
+    <>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
-    </div>
+    </>
   );
 }
